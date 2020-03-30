58e9659485cb86a79180a7869b92473feb22a369
Florencia Chomski <fchomski@hotmail.ca>
2020-03-30 17:43:54 -0500

    Profile Page Update

--- app/src/main/java/com/example/farmfresh/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/farmfresh/ui/profile/ProfileFragment.java
@@ -20,16 +20,16 @@ public class ProfileFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        profileViewModel =
-                ViewModelProviders.of(this).get(ProfileViewModel.class);
+//        profileViewModel =
+//                ViewModelProviders.of(this).get(ProfileViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        profileViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+//        final TextView textView = root.findViewById(R.id.text_home);
+//        profileViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
         return root;
     }
 }
--- app/src/main/java/com/example/farmfresh/ui/profile/ProfileViewModel.java
+++ app/src/main/java/com/example/farmfresh/ui/profile/ProfileViewModel.java
@@ -6,14 +6,14 @@ import androidx.lifecycle.ViewModel;
 
 public class ProfileViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
-
-    public ProfileViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is profile fragment");
-    }
-
-    public LiveData<String> getText() {
-        return mText;
-    }
+//    private MutableLiveData<String> mText;
+//
+//    public ProfileViewModel() {
+//        mText = new MutableLiveData<>();
+//        mText.setValue("This is profile fragment");
+//    }
+//
+//    public LiveData<String> getText() {
+//        return mText;
+//    }
 }
