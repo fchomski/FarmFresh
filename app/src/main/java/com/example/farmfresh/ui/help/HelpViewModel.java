093cb8279837d24438703ea3bda173d9a8d59cf3
Florencia Chomski <fchomski@hotmail.ca>
2020-04-01 20:40:27 -0500

    Addition and integration of nearme and help fragments

---
+++ app/src/main/java/com/example/farmfresh/ui/help/HelpViewModel.java
@@ -0,0 +1,7 @@
+package com.example.farmfresh.ui.help;
+
+import androidx.lifecycle.ViewModel;
+
+public class HelpViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
--- app/src/main/java/com/example/farmfresh/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/farmfresh/ui/profile/ProfileFragment.java
@@ -16,20 +16,9 @@ import com.example.farmfresh.R;
 
 public class ProfileFragment extends Fragment {
 
-    private ProfileViewModel profileViewModel;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-//        profileViewModel =
-//                ViewModelProviders.of(this).get(ProfileViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
-//        final TextView textView = root.findViewById(R.id.text_home);
-//        profileViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
         return root;
     }
 }
